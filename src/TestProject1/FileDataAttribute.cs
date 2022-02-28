c2316ce95c0344fbad4c8527386714426ceec5aa
logikfabrik <anton@logikfabrik.se>
2022-02-28 23:06:04 +0100

    Add test code

---
+++ src/TestProject1/FileDataAttribute.cs
@@ -0,0 +1,28 @@
+﻿namespace TestProject1
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Reflection;
+    using Xunit.Sdk;
+
+    public sealed class FileDataAttribute : DataAttribute
+    {
+        private readonly string _path;
+
+        public FileDataAttribute(string path)
+        {
+            _path = path;
+        }
+
+        public override IEnumerable<object[]> GetData(
+            MethodInfo testMethod)
+        {
+            if (File.Exists(_path))
+                yield return new object[] { File.ReadAllText(_path) };
+            else if (Directory.Exists(_path))
+                foreach (var path in Directory.EnumerateFiles(_path, "*.*",
+                             SearchOption.AllDirectories))
+                    yield return new object[] { File.ReadAllText(path) };
+        }
+    }
+}
---
+++ src/TestProject1/Workspaces/WorkspacesClientTests.cs
@@ -0,0 +1,20 @@
+﻿namespace TestProject1.Workspaces
+{
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public sealed class WorkspacesClientTests
+    {
+        [Theory]
+        [FileData(@".\Workspaces\.testdata\Get_For_Workspace_That_Exists_Should_Return_The_Workspace.json")]
+        public async Task Get_For_Workspace_That_Exists_Should_Return_The_Workspace(string responseJson)
+        {
+        }
+
+        [Theory]
+        [FileData(@".\Workspaces\.testdata\List_Should_Return_The_Workspaces.json")]
+        public async Task List_Should_Return_The_Workspaces(string responseJson)
+        {
+        }
+    }
+}
